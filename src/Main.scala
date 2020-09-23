9b68c24d71439f9eefc262b1e23639ba1438f863
nazarevitsch <bidaritter1997@gmail.com>
2020-09-24 01:00:24 +0300

    Start of the second lab.

--- src/Main.scala
+++ src/Main.scala
@@ -1,14 +1,22 @@
 import FirstLab.FirstLab._
+import SecondLab.FunSets._
+
 object Main {
 
+
+
     def main(args: Array[String]) {
-      print(exponent(2, 0) + "\n");
-      print(pascal(0, 2) + "\n")
-      print(pascal(1, 2) + "\n")
-      print(pascal(1, 3) + "\n")
-      print(pascal(5, 9) + "\n")
-      print(balance("(if (zero? x) max (/ 1 x))".toList) + "\n")
-      var a: List[Int] = List(1,2,3)
-      print(countChange(6, a))
+//      First Lab Tests
+//      print(exponent(2, 0) + "\n");
+//      print(pascal(0, 2) + "\n")
+//      print(pascal(1, 2) + "\n")
+//      print(pascal(1, 3) + "\n")
+//      print(pascal(5, 9) + "\n")
+//      print(balance("(if (zero? x) max (/ 1 x))".toList) + "\n")
+//      var a: List[Int] = List(1,2,3)
+//      print(countChange(6, a))
+
+//      Second Lab Tests
+
     }
 }
